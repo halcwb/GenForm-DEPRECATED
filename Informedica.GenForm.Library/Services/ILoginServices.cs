d37c03e48edc4d81f19edf340d59dd0cc4e894d9
Casper Bollen <halcwb@gmail.com>
2011-04-19 14:03:22 +0200

    Recreating project, making tests work again

---
+++ Informedica.GenForm.Library/Services/ILoginServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Informedica.GenForm.Library.Security;
+
+namespace Informedica.GenForm.Library.Services
+{
+    public interface ILoginServices
+    {
+        void Login(LoginUser user);
+        bool Logout(LoginUser user);
+        bool ChangePassword(LoginUser user, String newPassword);
+        bool IsLoggedIn(LoginUser user);
+        LoginUser GetLoginUser(String userName, String password);
+    }
+}
