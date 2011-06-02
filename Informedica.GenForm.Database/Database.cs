9f0aafd501d36ead6635107f7636edf5fad388b6
halcwb <halcwb@gmail.com>
2011-06-02 10:32:41 +0200

    Changed some database settings

--- Informedica.GenForm.Database/Database.cs
+++ Informedica.GenForm.Database/Database.cs
@@ -27,7 +27,6 @@ namespace Informedica.GenForm.Database
                     catch (Exception e)
                     {
                         // Temporary solution because Linqpad cannot locate app.config
-                        // connection = @"Data Source=INDURAIN;Initial Catalog=GenForm;User ID=***;Password=***";
                     }
                     break;
                 case DatabaseName.GenForm:
@@ -37,8 +36,8 @@ namespace Informedica.GenForm.Database
                     }
                     catch (Exception e)
                     {
-                        connection = @"Data Source=INDURAIN;Initial Catalog=GenForm;User ID=mvadmin;Password=sysimd";
-                        //connection = @"Data Source=HAL-WIN7\INFORMEDICA;Initial Catalog=GenForm;Integrated Security=True";
+                        //connection = @"Data Source=INDURAIN;Initial Catalog=GenForm;Integrated Security=True;
+                        connection = @"Data Source=HAL-WIN7\INFORMEDICA;Initial Catalog=GenForm;Integrated Security=True";
                     }
                     break;
                 default:
