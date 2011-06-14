60665980807aed2430c0d07578cb4e10df1ed4f3
halcwb <halcwb@gmail.com>
2011-06-14 16:11:47 +0200

    All tests pass, created an ext deployment file

--- Informedica.GenForm.Database/Database.cs
+++ Informedica.GenForm.Database/Database.cs
@@ -32,12 +32,12 @@ namespace Informedica.GenForm.Database
                 case DatabaseName.GenForm:
                     try
                     {
-                        connection = System.Configuration.ConfigurationManager.ConnectionStrings[GetConnectionName(database)].ConnectionString;
+                        connection = @"Data Source=INDURAIN;Initial Catalog=GenForm;User ID=genform; Password=genform";
                     }
                     catch (Exception e)
                     {
-                        connection = @"Data Source=INDURAIN;Initial Catalog=GenForm;User ID=genform; Password=genform";
-                        //connection = @"Data Source=HAL-WIN7\INFORMEDICA;Initial Catalog=GenForm;Integrated Security=True";
+                        // connection = @"Data Source=INDURAIN;Initial Catalog=GenForm;User ID=genform; Password=genform";
+                        connection = @"Data Source=HAL-WIN7\INFORMEDICA;Initial Catalog=GenForm;Integrated Security=True";
                     }
                     break;
                 default:
