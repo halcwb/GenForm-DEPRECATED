68a4467b6c9cbac65dee6889fcf164c183fcd1b7
halcwb <halcwb@gmail.com>
2011-07-06 16:08:55 +0200

    Implemented all use case tests to enter a new product & refactured test code.

---
+++ Informedica.GenForm.Mvc3/Client/GenForm/app/test/util/DomClicker.js
@@ -0,0 +1,18 @@
+Ext.define('GenForm.test.util.DomClicker', {
+
+    click: function (domelement) {
+        var me = this;
+        if (!domelement.click) {
+            me.createClick(domelement);
+        }  else {
+            domelement.click();
+        }
+    },
+
+    createClick: function (domelement) {
+        var mouseEvent = document.createEvent("MouseEvent");
+
+        mouseEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, 0, null);
+        domelement.dispatchEvent(mouseEvent);
+    }
+});
