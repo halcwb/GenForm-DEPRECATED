1e9a2aa0d53bd95a6d50be9c9bab9e1f2e31aed9
Casper Bollen <halcwb@gmail.com>
2012-02-01 19:18:03 +0100

    Implemented command line tool to write and get secure settings

---
+++ Informedica.GenForm.Acceptance/Utilities/Echo.cs
@@ -0,0 +1,10 @@
+﻿namespace Informedica.GenForm.Acceptance.Utilities
+{
+    public class Echo
+    {
+        public string EchoIt(string it)
+        {
+            return it;
+        }
+    }
+}
