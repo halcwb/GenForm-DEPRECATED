8319d61ded76fbce0a0b938d2c755e9808ae0119
Casper Bollen <halcwb@gmail.com>
2012-02-03 13:06:44 +0100

    Implemented secure setting manager using web.config as source to write and read connectionstrings

--- Informedica.GenForm.Settings/SettingsManager.cs
+++ Informedica.GenForm.Settings/SettingsManager.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading;
+using System.Web.Configuration;
 using System.Xml.Linq;
+using Informedica.SecureSettings;
 using StructureMap;
 
 namespace Informedica.GenForm.Settings
@@ -18,6 +22,12 @@ namespace Informedica.GenForm.Settings
 
         private readonly string _key = SecurityKey.Key;
         private readonly SymCryptography _crypt = new SymCryptography(SymCryptography.ServiceProviderEnum.Rijndael);
+        private SecureSettingsManager _secure;
+
+        public SettingsManager(SecureSettingsManager secureSettingsManager)
+        {
+            _secure = secureSettingsManager;
+        }
 
         private const string Environments = "environments";
         private const string EnvironmentName = "name";
@@ -32,7 +42,7 @@ namespace Informedica.GenForm.Settings
                 {
                     if (_instance == null)
                     {
-                        var instance = new SettingsManager();
+                        var instance = new SettingsManager(GetSecureSettingsManager());
                         Thread.MemoryBarrier();
                         _instance = instance;
                         //_instance.Initialize();
@@ -40,6 +50,16 @@ namespace Informedica.GenForm.Settings
                     return _instance;
                 }
             }
+        }
+
+        private static SecureSettingsManager GetSecureSettingsManager()
+        {
+            return new SecureSettingsManager(GetSettingSource());
+        }
+
+        private static ISettingSource GetSettingSource()
+        {
+           return new SettingSource();
         }
 
         #endregion
@@ -182,8 +202,86 @@ namespace Informedica.GenForm.Settings
 
         public string GetConnectionString(string environment)
         {
-            return "Data Source=:memory:;Version=3;New=True;Pooling=True;Max Pool Size=1;";
+            return _secure.GetConnectionString(environment);
         }
+
+        public void AddConnectionString(string name, string connectionString)
+        {
+            _secure.SetConnectionString(name, connectionString);
+        }
+    }
+
+    public class SettingSource: ISettingSource
+    {
+        private static IList<Setting> _settings = new List<Setting>();
+        private static Configuration _configuration = WebConfigurationManager.OpenWebConfiguration("/GenForm");
+
+        #region Implementation of IEnumerable
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public IEnumerator<Setting> GetEnumerator()
+        {
+            _settings.Clear();
+            foreach (ConnectionStringSettings connstr in _configuration.ConnectionStrings.ConnectionStrings)
+            {
+                _settings.Add(new Setting(connstr.Name, connstr.ConnectionString));
+            }
+            return _settings.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Implementation of ISettingSource
+
+        public void WriteConnectionString(string name, string connectionString)
+        {
+            var conset = _configuration.ConnectionStrings.ConnectionStrings[name];
+            if (conset == null)
+            {
+                conset = new ConnectionStringSettings(name, connectionString);
+                _configuration.ConnectionStrings.ConnectionStrings.Add(conset);
+            }
+            else
+            {
+                _configuration.ConnectionStrings.ConnectionStrings[name].ConnectionString = connectionString;
+            }
+            _configuration.Save();
+        }
+
+        public string ReadConnectionString(string name)
+        {
+            return _configuration.ConnectionStrings.ConnectionStrings[name].ConnectionString;
+        }
+
+        public void WriteAppSetting(string name, string setting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadAppSetting(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 
     public class SettingsManagerException : Exception
