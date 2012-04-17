bb22916ef23e37c1ceb6ace408dddd04489f2704
Casper Bollen <halcwb@gmail.com>
2012-04-17 11:18:12 +0200

    Implemented reading of appsetting

--- Informedica.GenForm.Settings.Tests/AConfigurationSettingSourceShould.cs
+++ Informedica.GenForm.Settings.Tests/AConfigurationSettingSourceShould.cs
@@ -13,27 +13,34 @@ namespace Informedica.GenForm.Settings.Tests
     {
         private SettingSource _configSettingSource;
         private Configuration _configarion;
+        private string _settingName;
+        private KeyValueConfigurationCollection _settings;
 
         [TestInitialize]
         public void IsolateTheWebSettingSource()
         {
+            _settingName = "Test";
             _configarion = Isolate.Fake.Instance<Configuration>();
-            Isolate.WhenCalled(() => _configarion.AppSettings).ReturnRecursiveFake();
-            Isolate.WhenCalled(() => _configarion.ConnectionStrings).ReturnRecursiveFake();
 
-            var writers = new Dictionary<Enum, Action<Setting>>();
-            var readers = new Dictionary<Enum, Func<string, Setting>>();
-            var removers = new Dictionary<Enum, Action<Setting>>();
-            _configSettingSource = new ConfigarionSettingSource(_configarion, writers, readers, removers);
+            _settings = Isolate.Fake.Instance<KeyValueConfigurationCollection>();
+            var fakeElement = Isolate.Fake.Instance<KeyValueConfigurationElement>();
+            Isolate.WhenCalled(() => fakeElement.Key).WillReturn(_settingName);
+            Isolate.WhenCalled(() => _settings[_settingName]).WillReturn(fakeElement);
+
+            Isolate.WhenCalled(() => _configarion.AppSettings.Settings).WillReturn(_settings);
+            Isolate.WhenCalled(() => _configarion.ConnectionStrings.ConnectionStrings).ReturnRecursiveFake();
+
+            _configSettingSource = new ConfigarionSettingSource(_configarion);
         }
 
+        [Isolated]
         [TestMethod]
         public void UseAConfigarionToGetTheAppSettings()
         {
             try
             {
                 _configSettingSource.Any();
-                Isolate.Verify.WasCalledWithAnyArguments(() => _configarion.AppSettings);
+                Isolate.Verify.WasCalledWithAnyArguments(() => _configarion.AppSettings.Settings);
 
             }
             catch (Exception e)
@@ -42,13 +49,14 @@ namespace Informedica.GenForm.Settings.Tests
             }
         }
 
+        [Isolated]
         [TestMethod]
         public void UseConfigurationToGetTheConnectionStrings()
         {
             try
             {
                 _configSettingSource.Any();
-                Isolate.Verify.WasCalledWithAnyArguments(() => _configarion.ConnectionStrings);
+                Isolate.Verify.WasCalledWithAnyArguments(() => _configarion.ConnectionStrings.ConnectionStrings);
 
             }
             catch (Exception e)
@@ -57,13 +65,34 @@ namespace Informedica.GenForm.Settings.Tests
             }
             
         }
+
+        [Isolated]
+        [TestMethod]
+        public void CallConfigationAppSettingsToReadAnAppSetting()
+        {
+            try
+            {
+                _configSettingSource.ReadSetting(ConfigarionSettingSource.Types.App, _settingName);
+                Isolate.Verify.WasCalledWithExactArguments(() => _settings[_settingName]);
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.ToString());
+            }
+        }
     }
 
     public class ConfigarionSettingSource : SettingSource
     {
-        private Configuration _configuration;
+        private readonly Configuration _configuration;
 
         #region Overrides of SettingSource
+
+        public ConfigarionSettingSource(Configuration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public ConfigarionSettingSource(Configuration configuration,
                                         IDictionary<Enum, Action<Setting>> writers, 
@@ -81,6 +110,7 @@ namespace Informedica.GenForm.Settings.Tests
 
         protected override void RegisterReaders()
         {
+            Readers.Add(Types.App, ReadAppSetting);
         }
 
         protected override void RegisterWriters()
@@ -95,24 +125,40 @@ namespace Informedica.GenForm.Settings.Tests
         {
             get
             {
-                var list = ReadAppSettings();
+                var list = new List<Setting>();
+                list.AddRange(ReadAppSettings());
                 list.AddRange(ReadConnectionStrings());
                 return list;
             }
         }
 
-        private List<Setting> ReadConnectionStrings()
+        private IEnumerable<Setting> ReadConnectionStrings()
         {
             return (from ConnectionStringSettings connstring in _configuration.ConnectionStrings.ConnectionStrings
                     select new Setting(connstring.Name, connstring.ConnectionString, "Conn", false)).ToList();
         }
 
-        private List<Setting> ReadAppSettings()
+        private IEnumerable<Setting> ReadAppSettings()
         {
             return (from KeyValueConfigurationElement setting in _configuration.AppSettings.Settings 
                     select new Setting(setting.Key, setting.Value, "App", false)).ToList();
         }
 
         #endregion
+
+        public Setting ReadAppSetting(string name)
+        {
+            return CreateSetting(_configuration.AppSettings.Settings[name]);
+        }
+
+        private static Setting CreateSetting(KeyValueConfigurationElement element)
+        {
+            return new Setting(element.Key, element.Value, Types.App.ToString(), false);
+        }
+
+        public enum Types
+        {
+            App
+        }
     }
 }
